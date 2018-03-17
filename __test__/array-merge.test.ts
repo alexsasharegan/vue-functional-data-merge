7e65a275f385f9ac02630466f936958b29603e74
Alex Regan <alex.joseph.regan@gmail.com>
2018-03-17 14:08:07 -0700

    chore(docs): adds explanation on scoped styles

--- __test__/array-merge.test.ts
+++ __test__/array-merge.test.ts
@@ -1,40 +1,49 @@
-import { VNodeData, VNodeDirective } from "vue"
-import { mergeData } from "../src/index"
+import { VNodeData, VNodeDirective } from "vue";
+import { mergeData } from "../src/index";
 
 it("should execute array merge on class, style, directive properties", () => {
-	let vd1: VNodeData = {
-		class: ["a", { b: true, c: false }],
-		style: ["display:block;", { color: "red", fontSize: "16px" }],
-	}
-	let vd2: VNodeData = {
-		class: ["d", { e: true, f: false }],
-		style: "position:absolute;",
-	}
+  function inserted(el: any) {
+    el.focus();
+  }
+  let vd1: VNodeData = {
+    class: ["a", { b: true, c: false }],
+    style: ["display:block;", { color: "red", fontSize: "16px" }],
+  };
+  let vd2: VNodeData = {
+    class: ["d", { e: true, f: false }],
+    style: "position:absolute;",
+    directives: [{ focus: { inserted } }],
+  };
 
-	let actual = mergeData(vd1, vd2)
-	let expected = {
-		class: ["d", { e: true, f: false }, "a", { b: true, c: false }],
-		style: ["position:absolute;", "display:block;", { color: "red", fontSize: "16px" }],
-	}
+  let actual = mergeData(vd1, vd2);
+  let expected = {
+    class: ["d", { e: true, f: false }, "a", { b: true, c: false }],
+    style: [
+      "position:absolute;",
+      "display:block;",
+      { color: "red", fontSize: "16px" },
+    ],
+    directives: [{ focus: { inserted } }],
+  };
 
-	// Check values recursively
-	expect(actual).toEqual(expected)
-	// Check that root object refs do not match
-	expect(actual).not.toBe(expected)
-	// Check that level 1 object refs do not match
-	expect(actual.class).not.toBe(vd1.class)
-	expect(actual.class).not.toBe(vd2.class)
-	expect(actual.style).not.toBe(vd1.style)
-	expect(actual.style).not.toBe(vd2.style)
-})
+  // Check values recursively
+  expect(actual).toEqual(expected);
+  // Check that root object refs do not match
+  expect(actual).not.toBe(expected);
+  // Check that level 1 object refs do not match
+  expect(actual.class).not.toBe(vd1.class);
+  expect(actual.class).not.toBe(vd2.class);
+  expect(actual.style).not.toBe(vd1.style);
+  expect(actual.style).not.toBe(vd2.style);
+});
 
 it("should init types to array", () => {
-	let test = mergeData({ class: "string" })
-	expect(Array.isArray(test.class)).toBe(true)
+  let test = mergeData({ class: "string" });
+  expect(Array.isArray(test.class)).toBe(true);
 
-	test = mergeData({ class: { string: true } })
-	expect(Array.isArray(test.class)).toBe(true)
+  test = mergeData({ class: { string: true } });
+  expect(Array.isArray(test.class)).toBe(true);
 
-	test = mergeData({ class: ["string"] })
-	expect(Array.isArray(test.class)).toBe(true)
-})
+  test = mergeData({ class: ["string"] });
+  expect(Array.isArray(test.class)).toBe(true);
+});
--- __test__/nullable-objects.test.ts
+++ __test__/nullable-objects.test.ts
@@ -1,12 +1,12 @@
-import { VNodeData, VNodeDirective } from "vue"
-import { mergeData } from "../src/index"
+import { VNodeData, VNodeDirective } from "vue";
+import { mergeData } from "../src/index";
 
 it("should handle nested nullable objects as initial state", () => {
-	let testData: VNodeData[] = [{ on: undefined }, { staticClass: "foo" }]
-	let actual: VNodeData
-	let boom = () => (actual = mergeData(...testData))
+  let testData: VNodeData[] = [{ on: undefined }, { staticClass: "foo" }];
+  let actual: VNodeData;
+  let boom = () => (actual = mergeData(...testData));
 
-	expect(boom).not.toThrowError()
-	expect(actual.staticClass).toBe("foo")
-	expect(actual.on).toEqual({})
-})
+  expect(boom).not.toThrowError();
+  expect(actual.staticClass).toBe("foo");
+  expect(actual.on).toEqual({});
+});
